6223c5c4e1b7fe324ec2424b1f3309c03fca6519
Janaína <janaina.engenharia1@gmail.com>
2021-12-04 16:46:03 -0300

    Organizing the code structure and orchestration

--- js/engine.js
+++ js/engine.js
@@ -1,12 +1,16 @@
 var mtx = [["", "", ""],["", "", ""],["", "", ""]]; // - Matrix of round game
-var typeOfMarker = 'X'; // - Flag of type of current marker
+var currentPlayer = 'X';
 
-function getTypeOfMarker(){
-	return typeOfMarker;
+function getCurrentPlayer(){
+	return currentPlayer;
 }
 
-function toggleTypeOfMarker(){
-	typeOfMarker == 'X' ? typeOfMarker = 'O' : typeOfMarker = 'X';
+function setCurrentPlayer(player){
+	currentPlayer = player;
+}
+
+function toggleCurrentPlayer(){
+	currentPlayer == 'X' ? currentPlayer = 'O' : currentPlayer = 'X';
 }
 
 function getMtx(){
@@ -17,24 +21,24 @@ function setMtx(value){
 	mtx = value;
 }
 
-function checkIfHasSequence(row, col){
+function hasSequence(coordinates){
 	var result;
 
 	//Check horizontal sequence
-	mtx[row][0] == mtx[row][1] && mtx[row][0] == mtx[row][2] ?  result = true : result = false;
+	mtx[coordinates.row][0] == mtx[coordinates.row][1] && mtx[coordinates.row][0] == mtx[coordinates.row][2] ?  result = true : result = false;
 
 	//Check vertical sequence
 	if(result == false){
-		mtx[0][col] == mtx[1][col] && mtx[0][col] == mtx[2][col] ?  result = true : result = false;
+		mtx[0][coordinates.col] == mtx[1][coordinates.col] && mtx[0][coordinates.col] == mtx[2][coordinates.col] ?  result = true : result = false;
 	}
 
 	//Check if has diagonal possibility
 	if(result == false){
-		if(!((row == 0 && col == 1) || (row == 1 && col == 0) || (row == 1 && col == 2) || (row == 2 && col == 1))){
-			if(row == col){
+		if(!((coordinates.row == 0 && coordinates.col == 1) || (coordinates.row == 1 && coordinates.col == 0) || (coordinates.row == 1 && coordinates.col == 2) || (coordinates.row == 2 && coordinates.col == 1))){
+			if(coordinates.row == coordinates.col){
 				mtx[0][0] == mtx[1][1] && mtx[0][0] == mtx[2][2] ? result = true : result = false;	
 			}
-			if(row+col == 2){
+			if(coordinates.row+coordinates.col == 2){
 				mtx[0][2] == mtx[1][1] && mtx[0][2] == mtx[2][0] ? result = true : result = false;	
 			}
 		}
--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function($) {
 	var canClick = true; // - Flag of allow click in player of game
 	var currentField; // - Current clicked field
+	var currentCoordinates = {col: "", row: ""};
 
-	function toggleCanClick(){
-		canClick ? canClick = false : canClick = true;
-		return canClick;
+	function getCurrentCoordinates(){
+		return currentCoordinates;
 	}
 
-	function getCanClick(){
-		return canClick;
+	function setCurrentCoordinates(coordinates){
+		currentCoordinates = coordinates;
 	}
 
 	function getCurrentField(){
@@ -17,49 +17,86 @@ $(document).ready(function($) {
 
 	function setCurrentField(clickedElement){
 		currentField = clickedElement;
+		setCurrentCoordinates({row: $(currentField).data("row"), col: $(currentField).data("col")});
 	}
 
+	function getCanClick(){
+		return canClick;
+	}
+
+	function toggleCanClick(){
+		canClick ? canClick = false : canClick = true;
+		return canClick;
+	}
+
+	function hasWinner(){
+		if(hasSequence(getCurrentCoordinates()) == true){
+			showWinner();
+			initGame();
+
+			return true;
+		}else{
+			return false;
+		}	
+	}
+
+	function showWinner(){
+		var resultModal = new bootstrap.Modal($('#resultModal'));
+		$("#resultModal p").html("Jogador " + getCurrentPlayer() + " ganhou!");
+		resultModal.show();
+	}
+
+	function initGame(){
+		initMatrizes();
+		setCurrentPlayer('X');
+	}
+
+	function initMatrizes(){
+		setMtx([["", "", ""],["", "", ""],["", "", ""]]); 
+		$("#player .field").each(function(){
+			$(this).html("");
+		});
+	}
+
+	function fillMatrix(){
+		var mtx = getMtx();
+
+		//Getting matrix's indexes of clicked field
+		var row = $(getCurrentField()).data("row");
+		var col = $(getCurrentField()).data("col");
+
+		//Fill the matrix with X or O
+		mtx[row][col] = getCurrentPlayer();
+	}
+
+	function fillInterfaceMatrix(element){
+		//Add the X or O into the field
+		$(element).html("<span class='marker'>" + getCurrentPlayer() + "</span>");
+	}
+
+	function isFilledField(){
+		var result;
+		$(getCurrentField()).contents().length == 0 ? result = false : result = true;
+		return result;
+	}
 
 	//Bind (listener) click of fields
 	$(".field").click(function(){
 		if(getCanClick()){
+			//Block the click
 			toggleCanClick();
 			setCurrentField(this);
 
 			//Check if field has content
-			if($(getCurrentField()).contents().length == 0){
-				//Add the X or O into the field
-				$(this).html("<span class='marker'>" + getTypeOfMarker() + "</span>");
-
-				//Ensure the correct synchrony between functions (Orchestration)
-				setTimeout(function(){
-					var mtx = getMtx();
-					var resultModal = new bootstrap.Modal($('#resultModal'));
-
-					//Getting matrix's indexes of clicked field
-					var row = $(getCurrentField()).data("row");
-					var col = $(getCurrentField()).data("col")
-
-					//Fill the matrix with X or O
-					mtx[row][col] = getTypeOfMarker();
-
-					//Check sequences of clicked field
-					result = checkIfHasSequence(row, col);
-
-					if(result == true){
-						$("#resultModal p").html("Jogador " + getTypeOfMarker() + " ganhou!");
-						resultModal.show();
-
-						setMtx([["", "", ""],["", "", ""],["", "", ""]]); 
-						$("#player .field").each(function(){
-							$(this).html("");
-						});
-					}
-
-					toggleTypeOfMarker();
-					toggleCanClick();
-				},500);
-
+			if(!isFilledField()){
+				fillInterfaceMatrix(this);
+				fillMatrix();
+				//If there isn't Winner
+				if(!hasWinner()){
+					toggleCurrentPlayer();
+				}
+				//Release the Click
+				toggleCanClick();
 			}else{
 				console.log("Campo Preenchido");	
 				toggleCanClick();
